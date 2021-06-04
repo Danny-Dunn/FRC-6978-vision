7f0ba2b0ceff35a1f917e9aede107213ce46e46f
Danny-Dunn <49325378+Danny-Dunn@users.noreply.github.com>
2021-06-04 19:08:56 -0400

    restructured examples

---
+++ examples/build/inrange/CMakeFiles/inrange.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inrange.dir/inrange.cpp.o"
+  "CMakeFiles/inrange.dir/inrange.cpp.o.d"
+  "inrange"
+  "inrange.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inrange.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/inrange/CMakeFiles/inrange.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for inrange.
+# This may be replaced when dependencies are built.
